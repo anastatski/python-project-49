a314f2fde1f924435cff1c17d61714a25c644787
Anastasiia <misssmith@bk.ru>
2025-08-22 15:19:36 +0300

    step 2

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,2 +1,12 @@
-def main():
+from brain_games.cli import welcome_user
+
+
+def greeting():
     print("Welcome to the Brain Games!")
+
+def main():
+    greeting()
+    welcome_user()
+
+if __name__ == "__main__":
+    main()
