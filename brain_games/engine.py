b188debb8e46a584c286cc26cfaf097e004f2b9d
Anastasiia <misssmith@bk.ru>
2025-08-22 16:27:42 +0300

    add asciinema & badges from SonarQube

---
+++ brain_games/engine.py
@@ -0,0 +1,22 @@
+import prompt
+
+
+def run_game(get_question_and_answer, instruction):
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}')
+    print(f'{instruction}')
+    attempts = 3
+
+    for _ in range(attempts):
+        question, correct_answer = get_question_and_answer()
+        print(f'Question: {question}')
+        user_answer = prompt.string('Your answer: ')
+
+        if user_answer == correct_answer:
+            print('Correct!')
+        else:
+            print(f"'{user_answer}' is wrong answer ;(. "
+                f"Correct answer was '{correct_answer}'. Let's try again, {name}!")
+
+    print(f'Congratulations, {name}!')
---
+++ brain_games/games/even.py
@@ -0,0 +1,22 @@
+
+import random
+
+from brain_games.engine import run_game
+
+EVEN_INSTRUCTION = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+
+def is_even(number):
+    return number % 2 == 0
+# функция-предикат: возвращает boolean значение!!!
+
+
+def get_num_and_answer_if_even() -> tuple:
+    random_number = random.randint(1, 100)
+    answer = 'yes' if is_even(random_number) else 'no'
+
+    return random_number, answer
+
+
+def run_even_game():
+    run_game(get_num_and_answer_if_even, EVEN_INSTRUCTION)
