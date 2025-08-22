418293d84486434af8f103d36d4722841e9ac09e
Anastasiia <misssmith@bk.ru>
2025-08-22 15:44:33 +0300

    step 4

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -4,9 +4,11 @@ from brain_games.cli import welcome_user
 def greeting():
     print("Welcome to the Brain Games!")
 
+
 def main():
     greeting()
     welcome_user()
+
 
 if __name__ == "__main__":
     main()
