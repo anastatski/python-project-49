4fc1316b13898b7be14efb6fb762300aa4dbc6a6
Anastasiia <misssmith@bk.ru>
2025-08-25 17:45:51 +0300

    fixing errors

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -3,7 +3,10 @@ import random
 
 from brain_games.engine import run_game
 
-EVEN_INSTRUCTION = 'Answer "yes" if the number is even, otherwise answer "no".'
+EVEN_INSTRUCTION = (
+                'Answer "yes" if the number is even, '
+                'otherwise answer "no".'
+)
 
 
 def is_even(number):
@@ -13,7 +16,7 @@ def is_even(number):
 
 
 def get_num_and_answer_if_even() -> tuple:
-    
+
     random_number = random.randint(1, 100)
     answer = 'yes' if is_even(random_number) else 'no'
 
