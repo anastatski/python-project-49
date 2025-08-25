c93633eca9b8ead6470cd00d35463a75d40f2239
Anastasiia <misssmith@bk.ru>
2025-08-25 16:18:12 +0300

    fixing errors

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -4,7 +4,9 @@ from brain_games.engine import run_game
 
 PRIME_INSTRUCTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
+
 def is_prime(number):
+    
     if number < 2:
         return False
     divider = 2
@@ -14,6 +16,7 @@ def is_prime(number):
         divider += 1
     return True
 
+
 def get_num_and_answer_if_prime():
     random_number = random.randint(1, 15)
     if is_prime(random_number):
@@ -22,6 +25,7 @@ def get_num_and_answer_if_prime():
         answer = 'no'
 
     return random_number, answer
+
 
 def run_prime_game():
     run_game(get_num_and_answer_if_prime, PRIME_INSTRUCTION)
--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -4,6 +4,7 @@ from brain_games.engine import run_game
 
 PROGRESSION_INSTRUCTION = 'What number is missing in the progression?'
 
+
 def get_progr_and_missing_num():
     start = random.randint(1, 50)
     step = random.randint(2, 10)
@@ -18,6 +19,7 @@ def get_progr_and_missing_num():
     progr_with_missed_num = ' '.join(map(str, progr))
 
     return progr_with_missed_num, str(missed_num)
+
 
 def run_progression_game():
     run_game(get_progr_and_missing_num, PROGRESSION_INSTRUCTION)
